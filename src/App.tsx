2c2b06f0a46f7351f4fed13f6edef870cbb2f530
[dyad] <albinause@gmail.com>
2026-04-26 10:13:55 +0200

    [dyad] Removing alcohol onboarding code from BelRealty app - wrote 2 file(s), deleted 6 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { isAppwriteConfigured } from "@/lib/appwrite";
 import Index from "./pages/Index";
 import PropertyDetail from "./pages/PropertyDetail";
 import Analytics from "./pages/Analytics";
 import Admin from "./pages/Admin";
 import Login from "./pages/Login";
 import Settings from "./pages/Settings";
-import AlcoholPage from "./pages/Alcohol";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient({
@@ -36,8 +34,7 @@ const App = () => (
           <Route path="/admin" element={<Admin />} />
           <Route path="/login" element={<Login />} />
           <Route path="/settings" element={<Settings />} />
-                    <Route path="/alcohol" element={<AlcoholPage />} />
-                    <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
